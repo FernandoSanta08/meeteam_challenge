5e6d4a5f91169fa49fb40fca6f75b37643850bea
Fernando Santa <santalopezfernando@gmail.com>
2024-07-29 20:12:56 -0500

    Failure implementation

--- meeting_challenge.rb
+++ meeting_challenge.rb
@@ -1,7 +1,7 @@
 require 'securerandom'
 
 class Node
-  attr_accessor :id, :log
+  attr_reader :id, :log, :failure
 
   def initialize(id)
     @uid = SecureRandom.uuid
@@ -9,6 +9,7 @@ class Node
     @log = []
     @neighbors = []
     @partitioned = []
+    @failure = false
   end
 
   def add_neighbor(node)
@@ -19,7 +20,7 @@ class Node
     neighbors = node_destination ? @neighbors.find{|neighbor| node_destination.include? neighbor.id} : @neighbors
     neighbors = [neighbors].compact unless neighbors.is_a?(Array)
     neighbors.each do |neighbor| 
-      next if @partitioned.include? neighbor
+      next if @partitioned.include? (neighbor) || neighbor.failure
       neighbor.receive_message(self, message)
       create_log("message sent from #{self.id} to #{neighbor.id}: #{message}")
     end
@@ -42,20 +43,32 @@ class Node
   def add_partitioned(node)
     @partitioned += node
   end
+
+  def node_failure
+    @failure = true
+    create_log("Failure in Node #{id}")
+  end
+
+
 end
+
+
 
 ### TEST
 
 node1 = Node.new(1)
 node2 = Node.new(2)
 node3 = Node.new(3)
+node4 = Node.new(4)
 node1.add_neighbor(node2)
 node1.add_neighbor(node3)
 node2.add_neighbor(node1)
 node2.add_neighbor(node3)
 node3.add_neighbor(node1)
 node3.add_neighbor(node2)
-node1.add_partitioned([node2, node3])
+node4.add_neighbor(node1)
+node1.add_neighbor(node3)
+node4.node_failure
+node1.add_partitioned([node3])
 
 node1.message("A", nil)
-puts node2.log
